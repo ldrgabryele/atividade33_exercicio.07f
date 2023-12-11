32c30368264cb7ba9a5186113ca1f5c609ac0448
gabs <146789686+ldrgabryele@users.noreply.github.com>
2023-12-11 14:18:16 -0300

    atividade33_exercicio.07f

---
+++ exercicio07-f.py
@@ -0,0 +1,6 @@
+#Crie um programa que leia o valor de um produto e imprima o
+#valor com desconto, tendo em vista que o desconto aplicado é de 10%.
+
+compra = float(input("Digite o valor da compra: "))
+
+def desconto (compra)
